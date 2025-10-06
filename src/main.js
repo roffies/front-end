1b0776472ff1edeaaaa92331849961e90924328e
Piero Sulca <psulcasanchez@gmail.com>
2025-10-05 21:11:05 -0500

    feat: integrate styles and i18n in main app

--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import { setupPrimeVue } from './shared-kernel/infrastructure/ui-framework/prime
 import { router } from './shared-kernel/infrastructure/router/router.js'
 import { i18n } from './shared-kernel/infrastructure/i18n/i18n.js'
 
-import './assets/main.css'
-import 'primeicons/primeicons.css'
+// Import all styles (variables, PrimeIcons, layout, utilities)
+import '@/assets/styles.scss'
 
 const app = createApp(App)
 
 setupPrimeVue(app, 'aura')
-
 app.use(router)
-
 app.use(i18n)
-
 app.mount('#app')
