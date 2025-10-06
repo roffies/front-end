89fd70bedd173b1ccf250b95f2b0e2689f72babf
Piero Sulca <psulcasanchez@gmail.com>
2025-10-05 21:10:36 -0500

    feat: configure http client with json server url

--- src/shared-kernel/infrastructure/http/http.js
+++ src/shared-kernel/infrastructure/http/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://api.smartcare.com'
+export const BASE_URL = 'http://localhost:3002'
 
 export const http = axios.create({
   baseURL: BASE_URL,
