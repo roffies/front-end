141c5398dfe28d9513d981e8340f37d93cbf114a
Piero Sulca <psulcasanchez@gmail.com>
2025-10-07 01:53:29 -0500

    feat: connect frontend to  API and remove local server

--- src/shared-kernel/infrastructure/http/http.js
+++ src/shared-kernel/infrastructure/http/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3002'
+export const BASE_URL = 'https://smartcare-api.up.railway.app'
 
 export const http = axios.create({
   baseURL: BASE_URL,
